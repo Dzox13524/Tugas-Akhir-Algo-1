0e5e0b83b56ec403dab712c633870af1d80464e5
Farewellez <ahmadzafarell55@gmail.com>
2024-11-22 23:20:30 +0700

    Menyalin semua isi file master untuk dilanjutkan farell di fitur masyarakat

--- main.py
+++ main.py
@@ -43,8 +43,15 @@ def registrasi():
         return 'password 1 dan 2 tidak sama'
 
 def login():
-    email = input('Masukkan Email: ')
-    password = input('Masukkan Password: ')
+    Clear_terminal()
+    print("""╭────〔 login menu 〕────☉
+│""")
+    email = input('├▷ Email: ')
+    print('│')
+    password = input('├▷ Masukkan Password: ')
+    print("""│
+╰────☉""")
+    input('Tekan enter untuk melanjutkan')
     database = pd.read_csv('./database.csv')
     if email in database['Email'].values:
         data = database[database['Email'] == email]
@@ -62,46 +69,155 @@ def login():
 def list_barangS():
     barang = pd.read_csv('./barangdanbahanS.csv')
     text = """
-▢---------------------------------------------------▢
-║              LIST BARANG DAN BAHAN SUBSIDI        ║
-▢---------------------------------------------------▢\n"""
+╔───────────────────────────────────────────────────╗
+║             LIST BARANG DAN BAHAN SUBSIDI         ║
+╠───────────────────────────────────────────────────╣\n"""
     index = 0
+    loop = 0
     for i in barang:
-        text+= f"│{i.upper()}" + ' '*(53-len(i)-2) + '│'+ '\n'
+        text+= f"║{i.upper()}" + ' '*(53-len(i)-2) + '║'+ '\n'
         for data in barang[i]:
             if str(data) == 'nan':continue
             else:
-                text += f"│  ▪ {str(data)}" + ' '*(53-len(str(data))-6) + '│'+ '\n'
+                text += f"║  ▪ {str(data)}" + ' '*(53-len(str(data))-6) + '║'+ '\n'
                 index+=1
-        text+= f"├---------------------------------------------------┤\n"
+        if loop < len(barang) :
+            text+= f"╠───────────────────────────────────────────────────╣\n"
+            loop += 1
+        else : 
+            text+= f"╚───────────────────────────────────────────────────╝\n"
     return text
 
-def menu(email):
+def menu(email, role):
+    Clear_terminal()
     data = pd.read_csv('./database.csv')
     data2 = data[data['Email'] == email].iloc[0].to_dict()
-    print(data2)
-    pass
-
-print(list_barangS())
-
+    pembuka = f'Selamat datang {data2['Name'].upper()} selamat berbelanja :>\n\n'
+    pembuka += f"""╔───────────────────────────────────────────────────╗
+║                   DATA INFORMASI                  ║
+╠───────────────────────────────────────────────────╣
+║ ⊳ Name   : {data2['Name'].upper() + ' '*(51-len(data2['Name']) - 12) + '║'}
+║ ⊳ Role   : {data2['Role'] + ' '*(51-len(data2['Role']) - 12) + '║'}
+║ ⊳ Lokasi : {data2['Daerah'] + ' '*(51-len(data2['Daerah']) - 12) + '║'}
+║ ⊳ Pesan  : 0                                      ║
+╠───────────────────────────────────────────────────╣\n"""
+    if role == 'admin':
+        pembuka += f"""║┌─────────────────────────────────────────────────┐║
+║│                    LIST MENU                    │║
+║├─────────────────────────────────────────────────┤║
+║├▶ 1. Feedback                                    │║
+║├▶ 2. E-Commerce                                  │║
+║├▶ 3. Inventaris Admin                            │║
+║├▶ 4. Log Out                                     │║
+║└─────────────────────────────────────────────────┘║
+╚───────────────────────────────────────────────────╝
+"""
+    elif role == 'user':
+        pembuka += f"""║┌─────────────────────────────────────────────────┐║
+║│                    LIST MENU                    │║
+║├─────────────────────────────────────────────────┤║
+║├▶ 1. Belanja                                     │║
+║├▶ 2. Keranjang                                   │║
+║├▶ 3. Log out                                     │║
+║└─────────────────────────────────────────────────┘║
+╚───────────────────────────────────────────────────╝
+"""
+    else :
+        pembuka += f"""║┌─────────────────────────────────────────────────┐║
+║│                    LIST MENU                    │║
+║├─────────────────────────────────────────────────┤║
+║├▶ 1. Request Barang                              │║
+║├▶ 2. Inventaris                                  │║
+║├▶ 3. Log Out                                     │║
+║└─────────────────────────────────────────────────┘║
+╚───────────────────────────────────────────────────╝
+"""
+    print(pembuka)
 
 # ===============
-# while True:
-#     print("""1. Registrasi\n2. Login\n3. Keluar""")
-#     # try:
-#     inputan = int(input('Masukkan nomor [1-3]: '))
-#     if inputan == 1:
-#             print(registrasi())
-#     elif inputan == 2:
-#             username, password = login()
-#             if username and password:
-#                 print('tes')
-#             else:
-#                 input('Tekan enter untuk melanjutkan!')
-#                 Clear_terminal()
-#     elif inputan == 3:
-#             break
-#     else :
-#         print('HARUS MEMASUKKAN YANG ADA DIATAS!')
-#     # except (ValueError) :
-#     #     print('Harus Angka!')
+while True:
+    print("""1. Registrasi\n2. Login\n3. Keluar""")
+    try:
+        inputan = int(input('Masukkan nomor [1-3]: '))
+        if inputan == 1:
+                print(registrasi())
+        elif inputan == 2:
+                username, role = login()
+                if username and role:
+                    while True:
+                        menu(username,role)
+                        if role == 'admin':
+                            pilihan = input('Pilih menu: ')
+                            match pilihan: 
+                                case '1':
+                                    Clear_terminal()
+                                    print("Feedback")
+                                    input('Tekan enter untuk melanjutkan!')
+                                case '2':
+                                    Clear_terminal()
+                                    print('E-Commerce')
+                                    input('Tekan enter untuk melanjutkan!')
+                                case '3':
+                                    Clear_terminal()
+                                    print('Inventaris Admin')     
+                                    input('Tekan enter untuk melanjutkan!')                           
+                                case '4':
+                                    Clear_terminal()
+                                    print('Log Out')     
+                                    input('Tekan enter untuk melanjutkan!')
+                                    break
+                                case ValueError:
+                                    Clear_terminal()
+                                    print('Harus angka 1-4')
+                                    input('Tekan enter untuk melanjutkan!')
+                        elif role == 'user':
+                            pilihan = input('Pilih menu: ')
+                            match pilihan:
+                                case '1':
+                                    Clear_terminal()
+                                    print('belanja') 
+                                    input('Tekan enter untuk melanjutkan!')
+                                case '2':
+                                    Clear_terminal()
+                                    print('keranjang') 
+                                    input('Tekan enter untuk melanjutkan!')
+                                case '3':
+                                    Clear_terminal()
+                                    print('log out') 
+                                    input('Tekan enter untuk melanjutkan!')
+                                    break
+                                case ValueError:
+                                    Clear_terminal()
+                                    print('Harus angka 1-3')
+                                    input('Tekan enter untuk melanjutkan!')
+                        else: 
+                            pilihan = input('Pilih menu: ')
+                            match pilihan:
+                                case '1':
+                                    Clear_terminal()
+                                    print(list_barangS()) 
+                                    input('Tekan enter untuk melanjutkan!')
+                                case '2':
+                                    Clear_terminal()
+                                    print('Inventaris') 
+                                    input('Tekan enter untuk melanjutkan!')
+                                case '3':
+                                    Clear_terminal()
+                                    print('log out') 
+                                    input('Tekan enter untuk melanjutkan!')
+                                    break
+                                case ValueError:
+                                    Clear_terminal()
+                                    print('Harus angka 1-3')
+                                    input('Tekan enter untuk melanjutkan!')
+                else:
+                    input('Tekan enter untuk melanjutkan!')
+                    Clear_terminal()
+        elif inputan == 3:
+                break
+        else :
+            print('HARUS MEMASUKKAN YANG ADA DIATAS!')
+    except (ValueError)  :
+        Clear_terminal()
+        print('Harus Angka!')
+        input('klik enter untuk melanjutkan')
