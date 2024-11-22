b5d29da3f3e7f1bb94d303296f43c892dacf99bc
Farewellez <ahmadzafarell55@gmail.com>
2024-11-22 11:26:49 +0700

    Sedikit mamahami code amat dan mencoba-coba

--- Fitur-Masyarakat.py
+++ Fitur-Masyarakat.py
@@ -1,1 +1,14 @@
-print("Hello World")
+list = ['Nanggroe Aceh Darussalam', 'Sumatera Utara', 'Sumatera Selatan', 'Sumatera Barat', 'Bengkulu', 'Riau', 'Kepulauan Riau', 'Jambi', 'Lampung', 'Bangka Belitung', 'Kalimantan Barat', 'Kalimantan Timur', 'Kalimantan Selatan', 'Kalimantan Tengah', 'Kalimantan Utara', 'Banten', 'DKI Jakarta', 'Jawa Barat', 'Jawa Tengah', 'Daerah Istimewa Yogyakarta', 'Jawa Timur', 'Bali', 'Nusa Tenggara Timur', 'Nusa Tenggara Barat', 'Gorontalo', 'Sulawesi Barat', 'Sulawesi Tengah', 'Sulawesi Utara', 'Sulawesi Tenggara', 'Sulawesi Selatan', 'Maluku Utara', 'Maluku', 'Papua Barat', 'Papua', 'Papua Tengah', 'Papua Pegunungan', 'Papua Selatan', 'Papua Barat Daya']
+for index, provinsi in enumerate(list, start= 1):
+    print(f"{index}. {provinsi}")
+
+while True:
+    try:
+        daerah = int(input('pilih daerah anda: '))
+        daerah -= 1
+        if 1 <= daerah:
+            print("YA")
+        else:
+            print("ya")
+    except ValueError:
+        print("anj")
